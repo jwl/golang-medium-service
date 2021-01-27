d3d80adb408fd9affaf4a1fef9c329cb0b419c7b
jwl <james.warren.leung@pm.me>
2021-01-26 20:20:36 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+From https://medium.com/swlh/create-go-service-the-easy-way-de827d7f07cf
+
+### Execution
+`go run main.go`
---
+++ homepage.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/sse"
+)
+
+// HomePage is the data container for the home page.
+type HomePage struct {
+	Time string
+}
+
+func serveHomepage(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	writingSync.Lock()
+	programIsRunning = true
+	writingSync.Unlock()
+
+	time.Sleep(10 * time.Second)
+
+	var homepage HomePage
+	homepage.Time = time.Now().String()
+
+	tmpl := template.Must(template.ParseFiles("html/homepage.html"))
+	_ = tmpl.Execute(writer, homepage)
+
+	writingSync.Lock()
+	programIsRunning = false
+	writingSync.Unlock()
+}
+
+func streamTime(timer *sse.Streamer) {
+	fmt.Println("Streaming time started")
+	for serviceIsRunning {
+		timer.SendString("", "time", time.Now().String())
+		time.Sleep(1 * time.Millisecond)
+	}
+}
