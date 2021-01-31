b1ec1673677b2b41a846557262a39b31d2c7d117
jwl <james.warren.leung@pm.me>
2021-01-31 10:42:57 -0800

    Completed up to end of part 4

--- homepage.go
+++ homepage.go
@@ -20,8 +20,6 @@ func serveHomepage(writer http.ResponseWriter, request *http.Request, params htt
 	programIsRunning = true
 	writingSync.Unlock()
 
-	time.Sleep(10 * time.Second)
-
 	var homepage HomePage
 	homepage.Time = time.Now().String()
 
--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,10 @@
 const testButton = document.getElementById("test-button")
+const time = new EventSource('/time');
 
 testButton.addEventListener("click", function () {
     alert("Button clicked")
 })
+
+time.addEventListener('time', (e) => {
+    document.getElementById("actual-time").innerHTML = e.data;
+}, false);
